0d608ac876e36136eff18fcab3de3f5d99bd309f
ryardley <r@blockhackers.io>
2026-02-16 02:36:23 +0000

    add enclave to flake

--- flake.nix
+++ flake.nix
@@ -16,13 +16,81 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       bb = barretenberg.packages.${system}.default;
+
+      platformMap = {
+        "x86_64-linux" = {
+          asset = "enclave-linux-x86_64.tar.gz";
+          hash = "sha256-D5jeY8YpuMAU/5iXIruZrmwuKNDBlf3nRH9H4Nr+kd4=";
+        };
+        "aarch64-darwin" = {
+          asset = "enclave-macos-aarch64.tar.gz";
+          hash = "sha256-XTU59pAk8GTEP86G7O0p8vLScVAnp6ruKV/CsUhm2rM=";
+        };
+      };
+
+      enclaveVersion = "0.1.14";
+
+      enclave = let
+        platform = platformMap.${system} or null;
+      in
+        if platform == null
+        then null
+        else
+          pkgs.stdenv.mkDerivation {
+            pname = "enclave";
+            version = enclaveVersion;
+
+            src = pkgs.fetchurl {
+              url = "https://github.com/gnosisguild/enclave/releases/download/v${enclaveVersion}/${platform.asset}";
+              hash = platform.hash;
+            };
+
+            sourceRoot = ".";
+
+            nativeBuildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux [
+              pkgs.autoPatchelfHook
+            ];
+
+            buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux [
+              pkgs.stdenv.cc.cc.lib
+              pkgs.openssl
+            ];
+
+            unpackPhase = ''
+              tar xzf $src
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              find . -name "enclave" -type f -executable | head -1 | xargs -I{} cp {} $out/bin/enclave \
+                || cp enclave $out/bin/enclave
+              chmod +x $out/bin/enclave
+            '';
+
+            dontBuild = true;
+            dontConfigure = true;
+
+            meta = with pkgs.lib; {
+              description = "Enclave CLI - encrypted execution environments";
+              homepage = "https://github.com/gnosisguild/enclave";
+              license = licenses.mit;
+            };
+          };
     in {
+      packages = pkgs.lib.optionalAttrs (enclave != null) {
+        enclave = enclave;
+        default = enclave;
+      };
+
       devShells.default = pkgs.mkShell {
-        packages = [
-          bb
-          pkgs.pkg-config
-          pkgs.openssl_3_6
-        ];
+        packages =
+          [
+            bb
+            pkgs.pkg-config
+            pkgs.openssl_3_6
+          ]
+          ++ pkgs.lib.optionals (enclave != null) [enclave];
+
         shellHook = ''
           export OPENSSL_DIR="${pkgs.openssl_3_6.dev}"
           export OPENSSL_LIB_DIR="${pkgs.openssl_3_6.out}/lib"
